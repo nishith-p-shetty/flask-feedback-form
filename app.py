f4f6778489912854faaddfd8d4a1d76f9c02e6ef
BhuvanSA <bhuvansaraj2003@gmail.com>
2024-09-02 13:52:59 +0530

    Add og

--- app.py
+++ app.py
@@ -8,38 +8,38 @@ from dotenv import load_dotenv
 load_dotenv()
 NO_OF_TEAMS = 3
 
-ADMIN_USERNAME = os.environ.get('FEEDBACK_FORM_ADMIN_USERNAME')
-ADMIN_PASSWORD = os.environ.get('FEEDBACK_FORM_ADMIN_PASSWORD')
-SECRET_KEY = os.environ.get('FEEDBACK_FORM_SECRET_KEY')
-DB_HOST = os.environ.get('FEEDBACK_FORM_DB_HOST')
-DB_NAME = os.environ.get('FEEDBACK_FORM_DB_NAME')
-DB_USER = os.environ.get('FEEDBACK_FORM_DB_USER')
-DB_PASSWORD = os.environ.get('FEEDBACK_FORM_DB_PASSWORD')
+ADMIN_USERNAME = os.environ.get("FEEDBACK_FORM_ADMIN_USERNAME")
+ADMIN_PASSWORD = os.environ.get("FEEDBACK_FORM_ADMIN_PASSWORD")
+SECRET_KEY = os.environ.get("FEEDBACK_FORM_SECRET_KEY")
+DB_HOST = os.environ.get("FEEDBACK_FORM_DB_HOST")
+DB_NAME = os.environ.get("FEEDBACK_FORM_DB_NAME")
+DB_USER = os.environ.get("FEEDBACK_FORM_DB_USER")
+DB_PASSWORD = os.environ.get("FEEDBACK_FORM_DB_PASSWORD")
 
 
 conn = psycopg2.connect(
-    host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME,
-    sslmode='prefer')
+    host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME, sslmode="prefer"
+)
 
 
 cursor = conn.cursor()
 
-cursor.execute('''
+cursor.execute("""
     CREATE TABLE IF NOT EXISTS reviewer (
         reviewer_id SERIAL PRIMARY KEY,
         reviewer_name VARCHAR(30),
         review_time TIMESTAMP
     );
-''')
+""")
 
-cursor.execute('''
+cursor.execute("""
     CREATE TABLE IF NOT EXISTS team (
         team_id SERIAL PRIMARY KEY,
         team_number INTEGER UNIQUE
     );
-''')
+""")
 
-cursor.execute('''
+cursor.execute("""
     CREATE TABLE IF NOT EXISTS feedbacks (
         feedback_id SERIAL PRIMARY KEY,
         reviewer_id INTEGER,
@@ -54,106 +54,129 @@ cursor.execute('''
         FOREIGN KEY (reviewer_id) REFERENCES reviewer(reviewer_id),
         FOREIGN KEY (team_id) REFERENCES team(team_id)
     );
-''')
+""")
 
-for team_id in range(1, NO_OF_TEAMS+1):
+for team_id in range(1, NO_OF_TEAMS + 1):
     cursor.execute(
-        ''' INSERT INTO team (team_number) VALUES (%s) ON CONFLICT (team_number) DO NOTHING''', (team_id,))
+        """ INSERT INTO team (team_number) VALUES (%s) ON CONFLICT (team_number) DO NOTHING""",
+        (team_id,),
+    )
 
 cursor.close()
 conn.commit()
 conn.close()
 
-app = Flask(__name__, template_folder='public/templates',
-            static_folder='public/static')
+app = Flask(__name__, template_folder="public/templates", static_folder="public/static")
 app.secret_key = SECRET_KEY
 CORS(app)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html', no_t=NO_OF_TEAMS)
+    return render_template("index.html", no_t=NO_OF_TEAMS)
 
 
-@app.route('/submit', methods=['POST'])
+@app.route("/submit", methods=["POST"])
 def submit():
     form_data = request.form
 
     conn = psycopg2.connect(
-        host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+        host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME
+    )
     cursor = conn.cursor()
 
-    cursor.execute('INSERT INTO reviewer (reviewer_name, review_time) VALUES (%s, %s) RETURNING reviewer_id',
-                   (form_data['reviewer_name'], dt.now()))
+    cursor.execute(
+        "INSERT INTO reviewer (reviewer_name, review_time) VALUES (%s, %s) RETURNING reviewer_id",
+        (form_data["reviewer_name"], dt.now()),
+    )
     reviewer_row = cursor.fetchone()
     reviewer_id = reviewer_row[0] if reviewer_row is not None else None
 
     feedbacks = []
-    for team_id in range(1, NO_OF_TEAMS+1):
-        field1_rating = int(form_data.get(
-            f'team_id{team_id}_field1_rating', 1))
-        field2_rating = int(form_data.get(
-            f'team_id{team_id}_field2_rating', 1))
-        field3_rating = int(form_data.get(
-            f'team_id{team_id}_field3_rating', 1))
-        field4_rating = int(form_data.get(
-            f'team_id{team_id}_field4_rating', 1))
-        field5_rating = int(form_data.get(
-            f'team_id{team_id}_field5_rating', 1))
+    for team_id in range(1, NO_OF_TEAMS + 1):
+        field1_rating = int(form_data.get(f"team_id{team_id}_field1_rating", 1))
+        field2_rating = int(form_data.get(f"team_id{team_id}_field2_rating", 1))
+        field3_rating = int(form_data.get(f"team_id{team_id}_field3_rating", 1))
+        field4_rating = int(form_data.get(f"team_id{team_id}_field4_rating", 1))
+        field5_rating = int(form_data.get(f"team_id{team_id}_field5_rating", 1))
 
-        feedback = form_data.get(f'team_id{team_id}_feedback')
+        feedback = form_data.get(f"team_id{team_id}_feedback")
 
         # cursor.execute('''''')
 
-        feedbacks.append((reviewer_id, team_id, field1_rating, field2_rating, field3_rating,
-                         field4_rating, field5_rating, field1_rating, field2_rating, field3_rating, field4_rating, field5_rating, feedback))
+        feedbacks.append(
+            (
+                reviewer_id,
+                team_id,
+                field1_rating,
+                field2_rating,
+                field3_rating,
+                field4_rating,
+                field5_rating,
+                field1_rating,
+                field2_rating,
+                field3_rating,
+                field4_rating,
+                field5_rating,
+                feedback,
+            )
+        )
 
-    cursor.executemany('''INSERT INTO feedbacks 
+    cursor.executemany(
+        """INSERT INTO feedbacks 
                        (reviewer_id, team_id, field1_rating, field2_rating, field3_rating, field4_rating, field5_rating, average_rating, feedback)
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, (SELECT AVG(s) FROM UNNEST(ARRAY[%s, %s, %s, %s, %s]) s), %s)''',
-                       feedbacks)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, (SELECT AVG(s) FROM UNNEST(ARRAY[%s, %s, %s, %s, %s]) s), %s)""",
+        feedbacks,
+    )
     conn.commit()
     cursor.close()
     conn.close()
 
-    return render_template('success.html', name=form_data['reviewer_name'])
+    return render_template("success.html", name=form_data["reviewer_name"])
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != ADMIN_USERNAME or request.form['password'] != ADMIN_PASSWORD:
-            error = 'Invalid Credentials. Please try again.'
+    if request.method == "POST":
+        if (
+            request.form["username"] != ADMIN_USERNAME
+            or request.form["password"] != ADMIN_PASSWORD
+        ):
+            error = "Invalid Credentials. Please try again."
         else:
-            session['logged_in'] = True
-            flash('Login Successful')
-            return redirect(url_for('dashboard'))
+            session["logged_in"] = True
+            flash("Login Successful")
+            return redirect(url_for("dashboard"))
 
-    return render_template('login.html', error=error)
+    return render_template("login.html", error=error)
 
 
-@app.route('/dashboard', methods=['GET', 'POST'])
+@app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
-    page = request.args.get('page', 1, type=int)
-    search = request.args.get('search', '', type=str).strip().lower()
+    page = request.args.get("page", 1, type=int)
+    search = request.args.get("search", "", type=str).strip().lower()
 
-    conn = psycopg2.connect(database=DB_NAME, user=DB_USER,
-                            password=DB_PASSWORD, host=DB_HOST)
+    conn = psycopg2.connect(
+        database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST
+    )
     cursor = conn.cursor()
 
     # Caluculate the total number of pages
-    cursor.execute('''SELECT COUNT(*) FROM feedbacks JOIN reviewer 
+    cursor.execute(
+        """SELECT COUNT(*) FROM feedbacks JOIN reviewer 
                     ON
                     feedbacks.reviewer_id = reviewer.reviewer_id
-                    WHERE LOWER(reviewer_name) LIKE %s''',
-                   ('%' + search + '%',))
+                    WHERE LOWER(reviewer_name) LIKE %s""",
+        ("%" + search + "%",),
+    )
     total_feedbacks = cursor.fetchone()
     total_feedbacks = total_feedbacks[0] if total_feedbacks is not None else 0
     total_pages = (total_feedbacks // 5) + 1
 
     # Fetch 5 feedbacks with reviewer and team details
-    cursor.execute('''
+    cursor.execute(
+        """
         SELECT 
             feedbacks.feedback_id,
             reviewer.reviewer_name,
@@ -172,11 +195,13 @@ def dashboard():
         WHERE LOWER(reviewer.reviewer_name) LIKE %s
         ORDER BY reviewer.review_time DESC 
         LIMIT 20 OFFSET %s;
-    ''', ('%' + search + '%', (page-1)*5))
+    """,
+        ("%" + search + "%", (page - 1) * 5),
+    )
 
     data = cursor.fetchall()
 
-    cursor.execute('''
+    cursor.execute("""
         (SELECT 'average_rating' AS rating_type, team_id, AVG(average_rating) AS rating
         FROM feedbacks
         GROUP BY team_id
@@ -206,55 +231,49 @@ def dashboard():
         FROM feedbacks
         GROUP BY team_id
         ORDER BY rating DESC);
-    ''')
+    """)
     best_teams = cursor.fetchall()
 
     cursor.close()
     conn.close()
 
     return render_template(
-        'dashboard.html',
+        "dashboard.html",
         best_teams=best_teams,
         data=data,
         total_pages=total_pages,
         page=page,
         search=search,
-        no_of_teams=NO_OF_TEAMS
+        no_of_teams=NO_OF_TEAMS,
     )
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('logged_in', None)
-    flash('Logout Successful')
-    return redirect(url_for('login'))
+    session.pop("logged_in", None)
+    flash("Logout Successful")
+    return redirect(url_for("login"))
 
 
-@app.route('/delete_feedback/<int:feedback_id>', methods=['GET'])
+@app.route("/delete_feedback/<int:feedback_id>", methods=["GET"])
 def delete_feedback(feedback_id):
-    if not session.get('logged_in'):
-        flash('Please login to delete feedback.')
-        return redirect(url_for('login'))
+    if not session.get("logged_in"):
+        flash("Please login to delete feedback.")
+        return redirect(url_for("login"))
 
-    conn = psycopg2.connect(database=DB_NAME, user=DB_USER,
-                            password=DB_PASSWORD, host=DB_HOST)
+    conn = psycopg2.connect(
+        database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST
+    )
     cursor = conn.cursor()
 
-    cursor.execute(
-        'DELETE FROM feedbacks WHERE feedback_id = %s', (feedback_id,))
+    cursor.execute("DELETE FROM feedbacks WHERE feedback_id = %s", (feedback_id,))
     conn.commit()
     cursor.close()
     conn.close()
 
-    flash('Feedback deleted successfully.')
-    return redirect(url_for('dashboard'))
+    flash("Feedback deleted successfully.")
+    return redirect(url_for("dashboard"))
 
 
-@app.route('/favicon.ico')
-def favicon():
-    # return redirect(url_for('static', filename='images/favicon.ico'))
-    return redirect(url_for('static', filename='favicon.ico'))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
